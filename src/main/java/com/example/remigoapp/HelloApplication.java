bd2e550a432c54c07dac26979c33ab9df32fe542
Sumyakhuu Ganbayar <34757946+bashuu@users.noreply.github.com>
2022-05-16 15:03:11 +0800

    added TimerTask, Manager class and scheduleAtFixedRate

--- src/main/java/com/example/remigoapp/HelloApplication.java
+++ src/main/java/com/example/remigoapp/HelloApplication.java
@@ -4,6 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.controlsfx.control.Notifications;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import java.io.IOException;
 
@@ -14,7 +21,14 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         user = new User("TestUser", "test@test.com", "testPass", 1,
                 false, "TestFName", "TestLName", 18, "Male");
+
+        List<MemoDate> memoDateList = new ArrayList<>();;
+        MemoDate memoDate = new MemoDate("TestMemo", "Testing", 1, LocalDate.now(), LocalDate.now(), LocalDate.now());
+        memoDateList.add(memoDate);
+        user.setMemoDateList(memoDateList);
+
         manager = new Manager(user);
+//        manager.startTimer();
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login_view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
--- src/main/java/com/example/remigoapp/Manager.java
+++ src/main/java/com/example/remigoapp/Manager.java
@@ -1,23 +1,63 @@
 package com.example.remigoapp;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.*;
 
 public class Manager {
     User currentUser = new User();
-    Timer timer  = new Timer ();
+    Timer timer  = new Timer();
+
     TimerTask task = new TimerTask() {
         @Override
         public void run() {
 
+            getDueMemoList();
+
         }
+
+
     };
 
     public Manager(User currentUser) {
         this.currentUser = currentUser;
     }
 
+    public Manager(){
+    };
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public void startTimer(){
+
+            timer.scheduleAtFixedRate(task,0,5000);
+    }
 
 
-    public Manager(){};
+    private Date convertLocalDate(LocalDate localDate){
+        //default time zone
+        ZoneId defaultZoneId = ZoneId.systemDefault();
+
+        //local date + atStartOfDay() + default time zone + toInstant() = Date
+        Date date = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant());
+
+        return date;
+    }
+
+    public void getDueMemoList(){
+        MemoDate memoDate;
+        List<MemoDate> memoDateList;
+        memoDateList = currentUser.getMemoDateList();
+        memoDate = memoDateList.get(0);
+        for(int i = 0; i < memoDateList.size(); i++){
+            if(memoDate.getNextRemindDate().isEqual(LocalDate.now())){
+
+                System.out.println(memoDate.getTitle() + " " + memoDate.getText());
+            }
+        }
+
+    }
+
 }
