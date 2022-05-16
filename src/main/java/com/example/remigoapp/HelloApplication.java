8c56972b121827e0e4354a35242b28081c217383
Sumyakhuu Ganbayar <34757946+bashuu@users.noreply.github.com>
2022-05-16 13:34:34 +0800

    Update HelloApplication.java

--- src/main/java/com/example/remigoapp/HelloApplication.java
+++ src/main/java/com/example/remigoapp/HelloApplication.java
@@ -8,8 +8,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    Manager manager;
+    User user;
     @Override
     public void start(Stage stage) throws IOException {
+        user = new User("TestUser", "test@test.com", "testPass", 1,
+                false, "TestFName", "TestLName", 18, "Male");
+        manager = new Manager(user);
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login_view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
