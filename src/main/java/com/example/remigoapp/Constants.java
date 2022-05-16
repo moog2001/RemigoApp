eb2a5aa7e0318e6586de30170cf8726750fefe95
moog2001 <moogforhire@gmail.com>
2022-05-16 15:35:26 +0800

    Constants

---
+++ src/main/java/com/example/remigoapp/Constants.java
@@ -0,0 +1,13 @@
+package com.example.remigoapp;
+
+public class Constants {
+    static public int TYPE_MEMO = 0;
+    static public int TYPE_MEMO_DATE = 1;
+    static public int TYPE_MEMO_DAILY = 2;
+    static public int TYPE_EDUCATION = 3;
+    static public String NAME_PACKAGE = "com.example.remigoapp";
+    static public String JCDBConnection = "jdbc:sqlite:com.example.remigoapp.db";
+    static public String JCDBClass = "org.sqlite.JDBC";
+
+
+}
---
+++ src/main/java/com/example/remigoapp/DatabaseHandler.java
@@ -0,0 +1,30 @@
+package com.example.remigoapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DatabaseHandler {
+
+
+    DatabaseHandler(){
+        connect();
+    }
+
+    private void connect() {
+        Connection c = null;
+
+        try {
+            Class.forName(Constants.JCDBClass);
+            c = DriverManager.getConnection(Constants.JCDBConnection);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+
+
+
+
+}
