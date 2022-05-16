56b0a1a05a5c3bedef203e3fb7b0c42e4eac0d3b
moog2001 <moogforhire@gmail.com>
2022-05-16 15:46:23 +0800

    commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.remigoapp {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.sql;
 
     opens com.example.remigoapp to javafx.fxml;
     exports com.example.remigoapp;
