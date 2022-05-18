4623e1902d76808302f3e7722ada90b9ff68cb91
moog2001 <moogforhire@gmail.com>
2022-05-18 21:45:31 +0800

    db

--- src/main/java/com/example/remigoapp/DatabaseHandler.java
+++ src/main/java/com/example/remigoapp/DatabaseHandler.java
@@ -1,104 +1,121 @@
 package com.example.remigoapp;
 
+import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class User extends Person {
-    protected String userName;
-    protected String eMail;
-    protected String password;
-    protected int userId;
-    private boolean isGuest;
-    private List<Memo> memoList = new ArrayList<>();
-    private List<MemoDate> memoDateList = new ArrayList<>();
 
-    public User(){
-    }
-    public User(String userName, String eMail, String password, int userId, boolean isGuest,
-                String firstName, String lastName, int age, String gender) {
-        this.userName = userName;
-        this.eMail = eMail;
-        this.password = password;
-        this.userId = userId;
-        this.isGuest = isGuest;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.gender = gender;
+public class DatabaseHandler {
+    Connection c = null;
+
+
+    DatabaseHandler() throws SQLException, ClassNotFoundException {
+        connect();
     }
 
-    public User(String userName, String eMail, String password, int userId, boolean isGuest) {
-        this.userName = userName;
-        this.eMail = eMail;
-        this.password = password;
-        this.userId = userId;
-        this.isGuest = isGuest;
+    private void connect() throws SQLException, ClassNotFoundException {
+            Class.forName(Constants.JCDB_CLASS);
+            c = DriverManager.getConnection(Constants.JCDB_CONNECTION);
     }
 
-    public User(String userName, String eMail, String password, int userId, boolean isGuest,
-                List<Memo> memoList, List<MemoDate> memoDateList) {
-        this.userName = userName;
-        this.eMail = eMail;
-        this.password = password;
-        this.userId = userId;
-        this.isGuest = isGuest;
-        this.memoList = memoList;
-        this.memoDateList = memoDateList;
+    public void resetAsGuest(){
+        
+    }
+
+    public User getUserData(String userIdInput) throws SQLException {
+        Statement statement = c.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from user where user_id="+userIdInput);
+        if(!resultSet.next()){
+            return null;
+        }
+        int userId = resultSet.getInt(1);
+        String firstName = resultSet.getString(2);
+        String lastName = resultSet.getString(3);
+        int age = resultSet.getInt(4);
+        String gender = resultSet.getString(5);
+        String userName = resultSet.getString(6);
+        String eMail = resultSet.getString(7);
+        int userMemoId = resultSet.getInt(8);
+        int userMemoDateId = resultSet.getInt(9);
+        String password = resultSet.getString(10);
+        boolean isGuest = resultSet.getBoolean(11);
+        statement.close();
+        List<Memo> memoList = getMemoList(userMemoId);
+        List<MemoDate> memoDateList = getMemoDateList(userMemoDateId);
+        return new User(userName,eMail,password,userId,isGuest,firstName,lastName,age,gender,memoList,memoDateList);
+
+
+
+    }
+
+    public List<MemoDate> getMemoDateList(int userMemoDateIdInput) throws SQLException {
+
+        List<MemoDate> memoDateList = new ArrayList<>();
+        Statement statement = c.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from user_memo_date where user_memo_id="+userMemoDateIdInput);
+        if(!resultSet.next()) {
+            return null;
+        }
+        int memoDateId = resultSet.getInt(3);
+        resultSet = statement.executeQuery("select * from memo where memo_date_id="+memoDateId);
+        while(resultSet.next()){
+            int type = resultSet.getInt(2);
+            String title = resultSet.getString(3);
+            String text = resultSet.getString(4);
+            LocalDate createDate = LocalDate.parse(resultSet.getString(5));
+            LocalDate lastRemindDate = LocalDate.parse(resultSet.getString(6));
+            LocalDate nextRemindDate = LocalDate.parse(resultSet.getString(7));
+            int interval = Constants.NULL_INT; // default null value
+            MemoDate memoDate;
+            switch (type){
+                case 2:{
+                    memoDate = new MemoDate(title,text,memoDateId,createDate,lastRemindDate,nextRemindDate);
+                    memoDateList.add(memoDate);
+                    break;
+                }
+                case 3:{
+                    interval = resultSet.getInt(8);
+                    memoDate = new MemoDaily(title,text,memoDateId,createDate,lastRemindDate,nextRemindDate, interval);
+                    memoDateList.add(memoDate);
+                }
+                case 4:{
+                    int streak = resultSet.getInt(9);
+                    memoDate = new Education(title,text,memoDateId,createDate,lastRemindDate,nextRemindDate,interval,streak);
+                    memoDateList.add(memoDate);
+                }
+            }
+
+        }
+        statement.close();
+        return memoDateList;
     }
 
 
-    public List<Memo> getMemoList() {
+    public List<Memo> getMemoList(int userMemoIdInput) throws SQLException {
+
+        List<Memo> memoList = new ArrayList<>();
+        Statement statement = c.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from user_memo where user_memo_id="+userMemoIdInput);
+        if(!resultSet.next()) {
+            return null;
+        }
+        int memoId = resultSet.getInt(3);
+        resultSet = statement.executeQuery("select * from memo where memo_id="+memoId);
+        while(resultSet.next()){
+            int type = resultSet.getInt(2);
+            String title = resultSet.getString(3);
+            String text = resultSet.getString(4);
+            LocalDate createDate = LocalDate.parse(resultSet.getString(5));
+            Memo memo = new Memo(title,text,memoId,createDate);
+            memoList.add(memo);
+        }
+        statement.close();
         return memoList;
     }
 
 
-    public void setMemoList(List<Memo> memoList) {
-        this.memoList = memoList;
-    }
 
-    public List<MemoDate> getMemoDateList() {
-        return memoDateList;
-    }
 
-    public void setMemoDateList(List<MemoDate> memoDateList) {
-        this.memoDateList = memoDateList;
-    }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEMail() {
-        return eMail;
-    }
-
-    public void setEMail(String eMail) {
-        this.eMail = eMail;
-    }
-
-    public String getPassword() { return password; }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public boolean isGuest() {
-        return isGuest;
-    }
-
-    public void setGuest(boolean guest) {
-        isGuest = guest;
-    }
 }
