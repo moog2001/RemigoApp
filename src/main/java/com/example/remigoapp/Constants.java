16b747fbcf3bca6c3b156963135a00ec61985e79
moog2001 <moogforhire@gmail.com>
2022-05-19 15:55:49 +0800

    constant and db reset

--- src/main/java/com/example/remigoapp/Constants.java
+++ src/main/java/com/example/remigoapp/Constants.java
@@ -1,13 +1,13 @@
 package com.example.remigoapp;
 
 public class Constants {
-    static public int TYPE_MEMO = 1;
-    static public int TYPE_MEMO_DATE = 2;
-    static public int TYPE_MEMO_DAILY = 3;
-    static public int TYPE_EDUCATION = 4;
-    static public String NAME_PACKAGE = "com.example.remigoapp";
-    static public String JCDB_CONNECTION = "jdbc:sqlite:com.example.remigoapp.db";
-    static public String JCDB_CLASS = "org.sqlite.JDBC";
-    static public int CHECK_INTERVAL = 6 * 1000 * 60 * 60 * 60;
-    static public int NULL_INT = -247;
+    static final public int TYPE_MEMO = 1;
+    static final public int TYPE_MEMO_DATE = 2;
+    static final public int TYPE_MEMO_DAILY = 3;
+    static final public int TYPE_EDUCATION = 4;
+    static final public String NAME_PACKAGE = "com.example.remigoapp";
+    static final public String JCDB_CONNECTION = "jdbc:sqlite:com.example.remigoapp.db";
+    static final public String JCDB_CLASS = "org.sqlite.JDBC";
+    static final public int CHECK_INTERVAL = 6 * 1000 * 60 * 60 * 60;
+    static final public int NULL_INT = -247;
 }
