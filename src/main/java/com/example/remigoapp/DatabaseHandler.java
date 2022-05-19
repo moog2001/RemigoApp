44a3970b0d03ad28137cac622348ae25e8be8837
moog2001 <moogforhire@gmail.com>
2022-05-19 14:08:46 +0800

    db insert and get data finished

--- src/main/java/com/example/remigoapp/DatabaseHandler.java
+++ src/main/java/com/example/remigoapp/DatabaseHandler.java
@@ -17,6 +17,9 @@ public class DatabaseHandler {
     private void connect() throws SQLException, ClassNotFoundException {
         Class.forName(Constants.JCDB_CLASS);
         c = DriverManager.getConnection(Constants.JCDB_CONNECTION);
+        Statement statement = c.createStatement();
+        statement.executeUpdate("PRAGMA foreign_keys = ON"); // this line allows cascade delete on the sql database
+//        resetDatabaseData("agreed");
         resetAsGuest();
     }
 
@@ -31,6 +34,7 @@ public class DatabaseHandler {
     public void resetAsGuest() throws SQLException {
         PreparedStatement statement = c.prepareStatement("delete from user where first_name='AS' and last_name='GUEST'");
         statement.executeUpdate();
+        statement.close();
 
         String firstName = "AS";
         String lastName = "GUEST";
@@ -40,15 +44,42 @@ public class DatabaseHandler {
         String eMail = "AS GUEST email";
         String password = "AS GUEST password";
         boolean isGuest = false;
+
+
         User asGuestUser = new User(userName,eMail,password,Constants.NULL_INT,isGuest,firstName,lastName,age,gender);
         int userId = createUser(asGuestUser);
         asGuestUser.setUserId(userId);
+
         Memo memoTest = new Memo("test title", "test text", Constants.NULL_INT, LocalDate.now());
         int memoTestId = createMemo(memoTest, userId);
         memoTest.setMemoId(memoTestId);
 
+        Education educationTest = new Education("test title", "test text", Constants.NULL_INT,LocalDate.now(), LocalDate.now(), LocalDate.now(),Constants.NULL_INT,Constants.NULL_INT);
+        int educationTestId = createMemoDate(educationTest, userId);
+        educationTest.setMemoId(educationTestId);
         User asGuestClone = getUserData(asGuestUser.getUserId()); // add breakpoint and debug to see the result.
 
+    }
+
+    /**
+     * use carefully
+     * @param password supply correct password
+     * @return
+     * @throws SQLException
+     */
+    public boolean resetDatabaseData(String password) throws SQLException {
+        if(!password.equals("agreed")){
+            return false;
+        }
+
+        Statement statement = c.createStatement();
+        statement.executeUpdate("delete from user;");
+        statement.executeUpdate("delete from memo;");
+        statement.executeUpdate("delete from memo_date;");
+        statement.executeUpdate("delete from user_memo;");
+        statement.executeUpdate("delete from user_memo_date;");
+        statement.close();
+        return true;
     }
 
 
@@ -76,6 +107,8 @@ public class DatabaseHandler {
             throw new SQLException("Could not get generated keys from AS GUEST");
         }
         int userId = resultSet.getInt(1);
+
+        statement.close();
         return userId;
     }
 
@@ -109,8 +142,43 @@ public class DatabaseHandler {
 
         int memoId = resultSet.getInt(1);
 //        memoInput.setMemoId(memoId);
+
+        statement.close();
         return memoId;
     }
+
+
+    public int createMemoDate(MemoDate memoDateInput, int userIdInput) throws SQLException {
+        PreparedStatement statement = c.prepareStatement("INSERT INTO memo_date(type,title,text,create_date,last_remind_date,next_remind_date,interval,streak,user_id) Values(?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+        int type = memoDateInput.getType();
+        statement.setInt(1, type);
+        statement.setString(2, memoDateInput.getTitle());
+        statement.setString(3, memoDateInput.getText());
+        statement.setString(4, memoDateInput.getCreateDate().toString());
+        statement.setString(5, memoDateInput.getLastRemindDate().toString());
+        statement.setString(6, memoDateInput.getNextRemindDate().toString());
+        if(type != 2) {
+            if (type == 4) {
+                statement.setInt(8, ((Education) memoDateInput).getStreak());
+            }
+            statement.setInt(7, ((MemoDaily)memoDateInput).getInterval());
+        }
+        statement.setInt(9, userIdInput);
+        int affectedRows = statement.executeUpdate();
+        if (affectedRows == 0) {
+            throw new SQLException("Count not insert into memo_date");
+        }
+        ResultSet resultSet = statement.getGeneratedKeys();
+        if (!resultSet.next()) {
+            throw new SQLException("could not get generated keys from memo_date");
+        }
+        int memoId = resultSet.getInt(1);
+//        memoInput.setMemoId(memoId);
+        statement.close();
+        return memoId;
+    }
+
 //
 //    private int createUserMemo(int userIdInput, int memoIdInput) throws SQLException {
 //        PreparedStatement statement = c.prepareStatement("INSERT INTO user_memo Values(?, ?)", Statement.RETURN_GENERATED_KEYS);
@@ -136,35 +204,34 @@ public class DatabaseHandler {
         if (!resultSet.next()) {
             return null;
         }
-        int userId = resultSet.getInt(1);
+        int userId = userIdInput;
         String firstName = resultSet.getString(2);
         String lastName = resultSet.getString(3);
         int age = resultSet.getInt(4);
         String gender = resultSet.getString(5);
         String userName = resultSet.getString(6);
         String eMail = resultSet.getString(7);
-        int userMemoId = resultSet.getInt(8);
-        int userMemoDateId = resultSet.getInt(9);
-        String password = resultSet.getString(10);
-        boolean isGuest = resultSet.getBoolean(11);
+        String password = resultSet.getString(8);
+        boolean isGuest = resultSet.getBoolean(9);
         statement.close();
-        List<Memo> memoList = getMemoList(userMemoId);
-        List<MemoDate> memoDateList = null; // getMemoDateList(userMemoDateId);
+        List<Memo> memoList = getMemoList(userId);
+        List<MemoDate> memoDateList = getMemoDateList(userId);
+//        List<MemoDate> memoDateList = null;
         return new User(userName, eMail, password, userId, isGuest, firstName, lastName, age, gender, memoList, memoDateList);
 
 
     }
 
-    public List<MemoDate> getMemoDateList(int userMemoDateIdInput) throws SQLException {
+    public List<MemoDate> getMemoDateList(int userIdInput) throws SQLException {
 
         List<MemoDate> memoDateList = new ArrayList<>();
         Statement statement = c.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from user_memo_date where user_memo_id=" + userMemoDateIdInput);
+        ResultSet resultSet = statement.executeQuery("select * from user_memo_date where user_id=" + userIdInput);
         if (!resultSet.next()) {
             return null;
         }
         int memoDateId = resultSet.getInt(3);
-        resultSet = statement.executeQuery("select * from memo where memo_date_id=" + memoDateId);
+        resultSet = statement.executeQuery("select * from memo_date where memo_date_id=" + memoDateId);
         while (resultSet.next()) {
             int type = resultSet.getInt(2);
             String title = resultSet.getString(3);
@@ -200,11 +267,11 @@ public class DatabaseHandler {
     }
 
 
-    public List<Memo> getMemoList(int userMemoIdInput) throws SQLException {
+    public List<Memo> getMemoList(int userIdInput) throws SQLException {
 
         List<Memo> memoList = new ArrayList<>();
         Statement statement = c.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from user_memo where user_memo_id=" + userMemoIdInput);
+        ResultSet resultSet = statement.executeQuery("select * from user_memo where user_id=" + userIdInput);
         if (!resultSet.next()) {
             return null;
         }
